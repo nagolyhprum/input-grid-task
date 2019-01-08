d860a8d26be1e93f4308afe9d5910b1f55b788a1
Logan Murphy <nagolyhprum@hotmail.com>
2019-01-07 22:49:42 -0500

    allow values to update

--- summing-grid/src/InputField.js
+++ summing-grid/src/InputField.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { setValue } from './actions/inputgrid'
+
 class InputField extends Component {
+  constructor(props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(e) {
+    const { value } = e.target
+    if(value) {
+      this.props.setValue(this.props.index, parseFloat(e.target.value))
+    }
+  }
+  onClick(e) {
+    e.target.select()
+  }
   render() {
     return (
       <div className="field">
-        <input value={this.props.value} type="number"/>
+        <input value={this.props.value} onChange={this.onChange} onClick={this.onClick} type="number"/>
       </div>
     )
   }
 }
 
 export default connect((state, props) => ({
-  value : state.inputgrid[props.index].value
-}))(InputField)
+  value : state.inputgrid[props.index].value,
+  index : props.index
+}), {
+  setValue
+})(InputField)
